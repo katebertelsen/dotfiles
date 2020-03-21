52a644e2f95b7c032bf60f3581640646d924be1a
Kate Bertelsen <bertelsk@wwt.com>
2020-03-20 22:15:11 -0500

    Merge git includes

--- includes/git.bash
+++ includes/git.bash
@@ -3,3 +3,6 @@
 alias gs='git status -sb'
 alias gd='git diff --no-ext-diff'
 alias gadd='git add -A && git status -sb'
+
+source /usr/local/etc/bash_completion.d/git-prompt.sh
+source /usr/local/etc/bash_completion.d/git-completion.bash
